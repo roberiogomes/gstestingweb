f8cb5de22ea34c51ea22104782a43c1861d5a2ad
Robério Gomes Patricio <roberiogomes@gmail.com>
2021-12-29 14:29:51 -0300

    Adding name param

--- src/main/java/com/example/gstestingweb/HomeController.java
+++ src/main/java/com/example/gstestingweb/HomeController.java
@@ -1,13 +1,22 @@
 package com.example.gstestingweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    @GetMapping(path = "/")
-    public @ResponseBody String greeting(){
-        return "Hello, world!";
+    @GetMapping(path = "/greeting")
+    public @ResponseBody String greeting(@RequestParam(required = false) String name){
+        String result = null;
+
+        if ((name == null) || name.isEmpty()){
+            result = "Hello, world!";
+        } else {
+            result = "Hello, " + name;
+        }
+
+        return result;
     }    
 }
