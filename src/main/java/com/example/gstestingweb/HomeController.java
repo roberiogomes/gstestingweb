e523e4fdc63c1231fabf32c29cc834ac80e9306a
Robério Gomes Patricio <roberiogomes@gmail.com>
2021-12-29 15:14:46 -0300

    Fixing sonar issues during code review

--- src/main/java/com/example/gstestingweb/HomeController.java
+++ src/main/java/com/example/gstestingweb/HomeController.java
@@ -1,20 +1,13 @@
 package com.example.gstestingweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
     @GetMapping(path = "/greeting")
-    public @ResponseBody String greeting(@RequestParam(required = false, defaultValue = "world") String name){
-        String encoded = null; 
-
-        if ((name != null) && (!name.isEmpty())){
-            encoded = name;
-        }
-        
-        return "Hello, " + encoded;
+    public @ResponseBody String greeting(){
+        return "Hello, world from Java";
     }    
 }
